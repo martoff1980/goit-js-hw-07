a3d8e60ff18165ccc517cfc8fe7348ba9f4113ed
martoff <martoff@ukr.net>
2025-01-20 01:33:14 +0200

    release 20.01.2025

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('#controls input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+function createBoxes(amount) {
+  boxesContainer.innerHTML = '';
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(input.value.trim(), 10);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    return;
+  }
+
+  input.value = '';
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
